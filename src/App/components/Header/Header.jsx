44df9b4eef8b864ab7bd9e727f27dd5487e7a0fb
Scott Serage <xttocsurge51@gmail.com>
2017-11-03 18:28:48 -0600

    played with css

--- src/App/components/Header/Header.jsx
+++ src/App/components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Header.css';
 
 export default function Header(props){
   return (
     <header id='header'>
-      Header
+      <div className='header-logo-temp'>logo</div>
+      <nav>
+        <a>Home</a>
+        <a>About</a>
+        <a>Factoids & Tools</a>
+        <a>Product Inquiries</a>
+        <a>Contact</a>
+      </nav>
     </header>
   );
 }
