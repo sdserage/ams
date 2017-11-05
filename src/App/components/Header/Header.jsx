1965bf4e8963a63d9625d10c53d9bb231be8bc51
Scott Serage <xttocsurge51@gmail.com>
2017-11-05 14:21:07 -0700

    Styled header, set up links, created a temporary logo

--- src/App/components/Header/Header.jsx
+++ src/App/components/Header/Header.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import './Header.css';
+import logo from './logo.svg';
+import {Link} from 'react-router-dom';
 
 export default function Header(props){
   return (
-    <header id='header'>
-      <div className='header-logo-temp'>logo</div>
-      <nav>
-        <a>Home</a>
-        <a>About</a>
-        <a>Factoids & Tools</a>
-        <a>Product Inquiries</a>
-        <a>Contact</a>
-      </nav>
-    </header>
+    <div>
+      <header id='header'>
+        <img src={logo} alt='logo'/>
+        <nav>
+          <Link to='/'>
+            <p>Home</p>
+          </Link>
+
+          <Link to='/about'>
+            <p>About</p>
+          </Link>
+
+          <Link to='/contact'>
+            <p>Contact</p>
+          </Link>
+
+          <Link to='/factoidsandtools'>
+            <p>Factoids & Tools</p>
+          </Link>
+
+          <Link to='/productinquiry'>
+            <p>Product Inquiry</p>
+          </Link>
+        </nav>
+      </header>
+      <div className='header-spacer'></div>
+    </div>
   );
 }
+//<div className='header-logo-temp'>logo</div>
