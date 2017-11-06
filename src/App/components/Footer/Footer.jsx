42b83da3bf78d35b5c1c1a04953cbba3bb470a30
Scott Serage <xttocsurge51@gmail.com>
2017-11-05 21:02:09 -0700

    Styled the footer

--- src/App/components/Footer/Footer.jsx
+++ src/App/components/Footer/Footer.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import './Footer.css';
 
 export default function Footer(props){
   return (
     <footer id='footer'>
-      Footer
+      <nav>
+        <div className='footer-box'>
+          <h3>Home</h3>
+          <p>About</p>
+          <p>Contact</p>
+        </div>
+        <div className='footer-box'>
+          <h3>Factoids & Tools</h3>
+        </div>
+        <div className='footer-box'>
+          <h3>Product Inquiry</h3>
+        </div>
+      </nav>
+      <h4 className='design-info'>Serage Web Design 2017</h4>
     </footer>
   );
 }
