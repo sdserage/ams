804cdb3e76e4a18bf6f6ece21b9c2a2ec33a6ad4
Scott Serage <xttocsurge51@gmail.com>
2017-11-07 14:07:05 -0700

    Added some content to About

--- src/App/components/Main/About/About.jsx
+++ src/App/components/Main/About/About.jsx
@@ -3,7 +3,28 @@ import React from 'react';
 export default function About(props){
   return (
     <section id='about'>
-      About
+      <h1>About</h1>
+
+      <p>
+        Air Management Specialists was established in 2006 with the objective to help fill some of the specific &amp;
+        critical needs of the ever growing industrial base for the once remote corners of Southern Utah,
+        Southern Nevada &amp; Northern Arizona. Gratefully, we can report that we have serviced, supplied &amp;
+        grown right beside many of the adventurous, dedicated and hard-working companies located in this
+        wonderful part of the southwest U.S. and, we look forward to continuing to be a part of the bright
+        future this area surely promises!
+      </p>
+
+      <p>
+        Air Management Specialists provides consulting services for the design, installation and/or upgrade of
+        industrial ventilation applications; this service includes the sale and service of Diversitech dust collectors
+        and industrial replacement air filters built in the U.S by Royal Filters.
+        Air Management Specialists also provides consulting services for the design, installation and/or upgrade
+        of industrial process applications; this service includes the sale and service of QSM industrial &amp; sanitary
+        ball valves, Jarecki severe service ball valves, Quaddax high performance butterfly valves, Max-Air
+        pneumatic &amp; electric valve actuators, solenoids, limit switches and positioners. Winter’s Instruments
+        gauges, diaphragm seals, thermometers and various other types of process instrumentation as well. Full
+        valve automation, assembly and testing is also a part of Air Management Specialists offering.
+      </p>
     </section>
   );
 }
