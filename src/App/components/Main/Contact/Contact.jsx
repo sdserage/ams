d44abc143280579f7e44833cf199f392c016bfec
Scott Serage <xttocsurge51@gmail.com>
2017-11-07 18:34:44 -0700

    Styled Contact

--- src/App/components/Main/Contact/Contact.jsx
+++ src/App/components/Main/Contact/Contact.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import './Contact.css';
 
 export default function Contact(props){
   return (
     <section id='contact'>
-      Contact
+      <h1 className='contact'>Contact</h1>
+      <h3>Air Management Specialists</h3>
+      <h5>Air Pollution Control / Process Control</h5>
+      <div className='contact-info'>
+        <p>tel: (xxx) xxx-xxxx</p>
+        <p>fax: (xxx) xxx-xxxx</p>
+        <p>email: xxxxxxxx@x.com</p>
+        <p>000 Xxxxxxxx Circle</p>
+        <p>xxxxx City, Utah 00000</p>
+      </div>
+
+      <a href='http://www.facebook.com/airmanagementspecialists' target='_blank'>
+        Like us on Facebook!
+      </a>
     </section>
   );
 }
