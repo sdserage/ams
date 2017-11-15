fa3d34b01a1982274197f69f331bd7fbdb38664f
Scott Serage <xttocsurge51@gmail.com>
2017-11-14 22:05:38 -0700

    Styled and implemented functionality for a cancel button

--- src/App/components/ProductInquiry/InquiryWizard/InquiryWizard.jsx
+++ src/App/components/ProductInquiry/InquiryWizard/InquiryWizard.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './InquiryWizard.css';
 /* Components */
 import FloatingActionButton from 'material-ui/FloatingActionButton';
+import RaisedButton from 'material-ui/RaisedButton';
 import {
   Card,
   CardActions,
@@ -13,19 +14,23 @@ import {
 import FontAwesome from 'react-fontawesome';
 
 export default class InquiryWizard extends Component{
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
       item: null
     };
   }
 
   render(){
+    const {cancel} = this.props;
     return(
-      <section className='inquiry-wizard'>
-        test
-
-      </section>
+      <Card className='inquiry-wizard' zDepth={3}>
+        <div className='wizard-grid'>
+          <div className='wizard-cancel'>
+            <RaisedButton secondary={true} label='Cancel' fullWidth={false} onClick={cancel}/>
+          </div>
+        </div>
+      </Card>
     );
   }
 };
