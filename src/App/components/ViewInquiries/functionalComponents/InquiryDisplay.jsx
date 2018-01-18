e9999a3c4cb474b2fabdc4b6e88609c009cd7db6
Scott Serage <xttocsurge51@gmail.com>
2018-01-17 22:35:51 -0700

    Gave icon button functionality

--- src/App/components/ViewInquiries/functionalComponents/InquiryDisplay.jsx
+++ src/App/components/ViewInquiries/functionalComponents/InquiryDisplay.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 /* Components */
 import {Card, CardText, CardTitle} from 'material-ui/Card';
+import AppBar from 'material-ui/AppBar';
 import ItemDisplay from './ItemDisplay';
+import IconButton from 'material-ui/IconButton';
+import KeyboardArrowDown from 'material-ui/svg-icons/hardware/keyboard-arrow-down';
+import KeyboardArrowUp from 'material-ui/svg-icons/hardware/keyboard-arrow-up';
+import Archive from 'material-ui/svg-icons/content/archive';
+import Unarchive from 'material-ui/svg-icons/content/unarchive';
+import Delete from 'material-ui/svg-icons/action/delete';
 
 const months = [
   'January',
@@ -46,24 +53,39 @@ function displayInquiryList
             inquiryContents[0] &&
             inquiryContents[0].inquiry_id === inquiry_id
           }
-          onClick={()=>{
-            if(viewInquiryContentsOn &&
-            inquiryContents[0] &&
-            inquiryContents[0].inquiry_id === inquiry_id){
-              updateInquiryContents(0);
-              inquiryContentsOff();
-            }else{
-              updateInquiryContents(inquiry_id);
-              inquiryContentsOn()
-            }
-          }}
         >
-          <CardText>
-            {`Date: ${months[standardDate.getMonth()]} ${standardDate.getDate()}, ${standardDate.getFullYear()}`}
-          </CardText>
-          <CardText>
-            {`Name: ${name}`}
-          </CardText>
+          <AppBar
+            title={`${name} - ${months[standardDate.getMonth()]} ${standardDate.getDate()}, ${standardDate.getFullYear()}`}
+            iconElementLeft={
+              <IconButton
+                onClick={()=>{
+                  if(viewInquiryContentsOn &&
+                  inquiryContents[0] &&
+                  inquiryContents[0].inquiry_id === inquiry_id){
+                    updateInquiryContents(0);
+                    inquiryContentsOff();
+                  }else{
+                    updateInquiryContents(inquiry_id);
+                    inquiryContentsOn()
+                  }
+                }}
+              >
+                {
+                  viewInquiryContentsOn &&
+                  inquiryContents[0] &&
+                  inquiryContents[0].inquiry_id === inquiry_id ?
+
+                  <KeyboardArrowUp/>
+                  :
+                  <KeyboardArrowDown/>
+                }
+              </IconButton>
+            }
+            iconElementRight={
+              <IconButton><Archive/></IconButton>
+            }
+
+          />
           <CardText>
             {`Email: ${email}`}
           </CardText>
